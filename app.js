1ff09a8f63b6f149b61d23c80c59758007d4427b
DimWitRM <DimWitRM@gmail.com>
2019-11-02 14:14:03 -0400

    Added port

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use( (err, req, res, next) => {
     res.render('./error');
 });
 
-app.listen(3000, () =>{
-    console.log('It is running in localhost:3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>{
+    console.log('It is running in localhost:'+ port);
 })
